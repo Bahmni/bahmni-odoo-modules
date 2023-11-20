e925e06e6132e397b1207f85874f39fdc945a7b5
Hari <hariprasath.a@kgisl.com>
2023-11-20 15:43:57 +0530

    [Hari] | BAH-3299 | Update. unwanted files removed

--- bahmni_api_feed/models/__init__.py
+++ bahmni_api_feed/models/__init__.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
 from . import api_event_worker
-from . import event_records
-from . import reference_data_service
-from . import drug_data_service
-from . import product_uom_service
 from . import res_company
 from . import res_users
 from . import order_type
 from . import syncable_units_mapping
-from . import api_feed_marker
 from . import order_save_service
 from . import order_type_shop_map
