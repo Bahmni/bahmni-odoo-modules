315c3b33b953226e4345768633bb317f1bfe9579
Hari <hariprasath.a@kgisl.com>
2023-11-22 10:15:14 +0530

    [Hari] | BAH-3305 | Add. Bahmni_product: Custom Fields and Logic for Product Enhancement

---
+++ bahmni_product/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import product_category
+from . import product_uom
+from . import res_partner
+from . import product_supplierinfo
+from . import product
---
+++ bahmni_product/models/product_category.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import uuid
+from odoo import models, fields, api
+
+
+class ProductCategory(models.Model):
+    _inherit = 'product.category'
+
+    uuid = fields.Char(string="UUID")
+
+    @api.model
+    def create(self, vals):
+        if vals.get('uuid') is None or not vals.get('uuid'):
+            vals.update({'uuid': uuid.uuid4()})
+        return super(ProductCategory, self).create(vals)
+
