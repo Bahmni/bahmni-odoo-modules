a5f7e60d869bce58cc3419f19206913c2624ca73
Hari <hariprasath.a@kgisl.com>
2023-11-23 10:12:18 +0530

    [Hari] | Add. Custom Modules for Testing

--- bahmni_api_feed/models/__init__.py
+++ bahmni_api_feed/models/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from . import api_event_worker
+from . import event_records
 from . import reference_data_service
+from . import product_uom_service
 from . import drug_data_service
 from . import res_company
 from . import res_users
