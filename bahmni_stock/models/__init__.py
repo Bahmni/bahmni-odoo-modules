6a6292700bf808f3999520f0f07fd04bef8020c4
Hari <hariprasath.a@kgisl.com>
2023-12-01 15:48:13 +0530

    [Hari] | BAH-3307 | Add. Customized bahmni_stock module

---
+++ bahmni_stock/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import stock_production_lot
+from . import stock_picking
+from . import stock_move
+from . import product
+from . import account_invoice_line
+from . import account_payment
