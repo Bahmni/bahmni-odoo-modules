dcecdde3062074b4304b7fdfd97d0ef26c1817d2
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2024-02-15 15:31:18 +0530

    BAH-3592 | Rename jss_data_import module as openerp7_data_import (#50)

--- openerp7_data_import/__init__.py
+++ openerp7_data_import/__init__.py
@@ -3,4 +3,4 @@ from odoo import api, SUPERUSER_ID
 
 def _create_demo_config_param(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    env['ir.config_parameter'].set_param('jss_data_import.admin', 'admin')
+    env['ir.config_parameter'].set_param('openerp_data_import.admin', 'admin')
