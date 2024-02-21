d78458cb9349d16bd3251de544ec0c4e477fe847
Karthikeyan <karthikeyansp91@gmail.com>
2024-02-21 19:10:41 +0530

    [Karthi] | BAH-3589 | Added. Fields changes has been added.

--- bahmni_purchase/models/price_markup_table.py
+++ bahmni_purchase/models/price_markup_table.py
@@ -6,19 +6,19 @@ from odoo.exceptions import UserError, ValidationError, AccessError, RedirectWar
 class PriceMarkupTable(models.Model):
     _name = 'price.markup.table'
 
-    lower_price = fields.Float(string="Lower Price", default=1)
-    higher_price = fields.Float(string="Higher Price", default=1)
+    lower_price = fields.Float(string="Minimum Cost", default=1)
+    higher_price = fields.Float(string="Maximum Cost", default=1)
     markup_percentage = fields.Float(string="Markup Percentage", default=1)
 
     @api.constrains('lower_price', 'higher_price')
     def _check_fields_values(self):        
         if self.lower_price > self.higher_price:
-            raise ValidationError('Lower Price should not be greater than Higher Price.')
+            raise ValidationError('Minimum cost should not be greater than maximum cost.')
         # Add any other conditions you need to check        
         for data in self.env['price.markup.table'].search([]):           
             if data.lower_price <= self.lower_price <= data.higher_price and data.id != self.id:
-                raise ValidationError('Your lower price is available within the range of Lower Price and Higher Price of previous records.')
+                raise ValidationError('Your minimum cost is available within the range of minimum cost and maximum cost of previous records.')
                 
             if data.higher_price <= self.higher_price <= data.higher_price and data.id != self.id:
-                raise ValidationError('Your higher price is available within the range of Lower Price and Higher Price of previous records.')
+                raise ValidationError('Your maximum cost is available within the range of minimum cost and maximum cost of previous records.')
                 
