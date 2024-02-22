fdb87df7176113bcef28315e5f92526289fcc99a
MOHANKUMAR T <mohan13081999@gmail.com>
2024-02-22 15:11:50 +0530

    BAH-3521 | Add new module bahmni_address_mapping

---
+++ bahmni_address_mapping/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Bahmni Address Mapping',
+    'version': '1.0',
+    'summary': 'Module to add additional fields to the address model',
+    'sequence': 1,
+    'description': """
+Bahmni Address Mapping
+====================
+""",
+    'category': 'Services',
+    'website': '',
+    'images': [],
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
