e58e2638479a67f38219ba3c65ccd5099f366a5c
Karthikeyan <karthikeyansp91@gmail.com>
2024-03-05 14:24:19 +0530

    [Karthi] | BAH-3549 | Refactor. Unwanted code has been removed

--- bahmni_reports/models/models.py
+++ bahmni_reports/models/models.py
@@ -1,18 +1,1 @@
-# -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
-
-
-# class bahmni_reports(models.Model):
-#     _name = 'bahmni_reports.bahmni_reports'
-#     _description = 'bahmni_reports.bahmni_reports'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+## New module added in this file
