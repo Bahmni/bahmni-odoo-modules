7a8f06a494a0acc2aefc91c415745fa9b7746711
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-03-12 08:01:23 +0530

    [Rahul] | BAH-3505 | Add. Bahmni Address Seed Initializer

---
+++ bahmni_initializer/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Bahmni Initializer",
+    'version': '0.1',
+    'summary': """
+        Module to read seed data from CSV/XML files and initialize the Bahmni Meta Data""",
+    'description': """
+Bahmni Initializer
+==================
+    """,
+    'author': "Bahmni",
+    'website': "https://www.yourcompany.com",
+    'category': 'Services',
+    'license': 'LGPL-3',
+    'depends': ['base','bahmni_address_mapping'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/address.seed.csv',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
