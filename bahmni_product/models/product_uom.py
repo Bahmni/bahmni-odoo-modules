452af7a5e33137b3485c60c26c8799f20f2d6e64
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-03-18 07:43:40 +0530

    [Rahul] | BAH-3696 | Add. Odoo Units Initializer

--- bahmni_product/models/product_uom.py
+++ bahmni_product/models/product_uom.py
@@ -1,7 +1,9 @@
 import uuid
 
 from odoo import models, fields, api
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class ProductUom(models.Model):
     _inherit = 'uom.uom'
@@ -10,14 +12,18 @@ class ProductUom(models.Model):
 
     @api.model
     def create(self, vals):
-        if vals.get('uuid') is None or not vals.get('uuid'):
-            vals.update({'uuid': uuid.uuid4()})
-        return super(ProductUom, self).create(vals)
+        try:
+            if vals.get('uuid') is None or not vals.get('uuid'):
+                vals.update({'uuid': uuid.uuid4()})
+            record = super(ProductUom, self).create(vals)
+            _logger.info("Created New Unit of Measure {} With Id {}.".format(*(vals.get("name"), record.id)))
+            return record
+        except Exception as error:
+            _logger.error("Failed To Create New Unit of Measure {}".format(vals.get("name")))
 
-# need to override this method to reverse sync updated data
+    # need to override this method to reverse sync updated data
     def write(self, vals):
         return super(ProductUom, self).write(vals)
-
 
 class ProductUomCategory(models.Model):
     _inherit = 'uom.category'
