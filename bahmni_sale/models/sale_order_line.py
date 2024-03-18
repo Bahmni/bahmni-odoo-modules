eb365c148a90ec204c293fcf04a3a95e78a8d313
Karthikeyan <karthikeyansp91@gmail.com>
2024-03-18 20:54:49 +0530

    [Karthi] | BAH-3662 | Refactor. Confirming and Qty change the sale order the unit price has been modify issue has been fixed (#104)

--- bahmni_sale/models/sale_order_line.py
+++ bahmni_sale/models/sale_order_line.py
@@ -47,6 +47,30 @@ class SaleOrderLine(models.Model):
         if self.product_id:           
            self.lot_id = self.get_available_batch_details(self.product_id,self.order_id.id)
            
+    @api.depends('product_id', 'product_uom', 'product_uom_qty')
+    def _compute_price_unit(self):
+        for line in self:
+            # check if there is already invoiced amount. if so, the price shouldn't change as it might have been
+            # manually edited
+            if line.qty_invoiced > 0:
+                continue
+            if not line.product_uom or not line.product_id or not line.order_id.pricelist_id:
+                line.price_unit = 0.0
+            else:
+                price = line.with_company(line.company_id)._get_display_price()
+                if bool(self.env['ir.config_parameter'].sudo().get_param('bahmni_sale.sale_price_markup')) == True:
+                    price = self.lot_id.sale_price if self.lot_id.sale_price > 0.0 else self.product_id.lst_price
+                line.price_unit = line.product_id._get_tax_included_unit_price(
+                    line.company_id,
+                    line.order_id.currency_id,
+                    line.order_id.date_order,
+                    'sale',
+                    fiscal_position=line.order_id.fiscal_position_id,
+                    product_price_unit=price,
+                    product_currency=line.currency_id
+                )
+    
+           
 
     @api.model
     def get_available_batch_details(self, product_id, sale_order):
