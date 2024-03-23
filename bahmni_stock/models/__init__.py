d62d50f098ac2d397615493ebfb3564c73f9173b
Karthikeyan <karthikeyansp91@gmail.com>
2024-03-23 08:29:59 +0530

    [Karthi] | BAH-3614 | Refactor. Delivery based Customer Item return process issue has been fixed based on location mapping.

--- bahmni_stock/models/__init__.py
+++ bahmni_stock/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import stock_production_lot
 from . import stock_picking
-from . import stock_move
 from . import stock_move_line
 from . import product
 from . import account_invoice_line
