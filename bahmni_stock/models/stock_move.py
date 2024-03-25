dcc7bfe2388201414dcac561f7858b8a61f02e23
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2024-03-25 10:32:36 +0530

    Revert "[Karthi] | BAH-3614 | Refactor. Delivery based Customer Item return p…"
    This reverts commit 3765ed55fbb5ad3c45694beafc5f2c94faaf75c5.

---
+++ bahmni_stock/models/stock_move.py
@@ -0,0 +1,19 @@
+
+from datetime import datetime
+from dateutil import tz
+
+from odoo import models, fields, api
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+    
+    
+    stock_picking_time = fields.Datetime(string="Stock Picking_time", store=True)
+    @api.model
+    def create(self,vals):
+        if vals.get('origin'):
+            sale_order = self.env['sale.order'].search([('name','=',str(vals.get('origin')))])
+            if any(sale_order) and sale_order.location_id:
+                vals.update({'location_id':sale_order.location_id.id})
+        return super(StockMove,self).create(vals)
