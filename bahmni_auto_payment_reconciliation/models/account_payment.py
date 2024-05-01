efcf99727733505ece3fb2edc6affe909da4efb6
MOHANKUMAR T <mohan13081999@gmail.com>
2024-05-01 23:40:26 +0530

    BAH-3748 | Refactor. Re-order function definitions to improve readability

--- bahmni_auto_payment_reconciliation/models/account_payment.py
+++ bahmni_auto_payment_reconciliation/models/account_payment.py
@@ -39,114 +39,6 @@ class AccountPayment(models.Model):
                 and not self.is_internal_transfer \
                 and self.env.context.get('default_partner_type') == 'customer'
 
-    def multi_invoice_search(self):
-        """ Using ref find the invoice obj """
-        return self.env['account.move'].search(
-            [('move_type', '=', 'out_invoice'), ('amount_residual', '>', 0), ('partner_id', '=', self.partner_id.id)],
-            order="id desc", limit=1)
-
-    def generate_report_action(self):
-        return self.env.ref("bahmni_multi_payment.account_summarized_multi_invoices_payment").report_action(self)
-
-    def cashier_name(self):
-        words = self.create_uid.name.split()
-        initials = ''.join(word[0].upper() for word in words)
-        return initials
-
-    def total_credit(self):
-        credit_value = 0.0
-        if self.partner_id:
-            self._cr.execute("""select sum(ABS(amount_residual)) from account_move where 
-                          (amount_residual < 0 or (move_type='out_refund' and amount_residual > 0)) and partner_id = %s
-                          """, (self.partner_id.id,))
-            result = self._cr.fetchall()
-            if result[0][0] is not None:
-                credit_value = result[0][0]
-            else:
-                credit_value = 0.00
-        return credit_value
-
-    def total_outstanding(self):
-        outstanding = 0.0
-        if self.partner_id:
-            self._cr.execute("""select sum(amount_residual) from account_move where 
-                              amount_residual > 0 and move_type='out_invoice' and partner_id = %s
-                              """, (self.partner_id.id,))
-            outstanding_value = self._cr.fetchall()
-            if outstanding_value[0][0] is not None:
-                outstanding = outstanding_value[0][0]
-            else:
-                outstanding = 0.00
-        return outstanding
-
-    def total_receivable(self):
-
-        return self.total_outstanding() - self.total_credit()
-
-    def action_post(self):
-        res = super(AccountPayment, self).action_post()
-        if self.is_auto_reconciliation_applicable:
-            if len(self.line_payment_invoice_debit_ids) > 0:
-                self.assign_credit_invoices_to_outstanding_invoices()
-            unprocessed_outstanding_invoices = self.get_unprocessed_outstanding_invoices()
-            if unprocessed_outstanding_invoices and self.amount > 0 and self.payment_type == 'inbound':
-                self.assign_payment_to_outstanding_invoices(unprocessed_outstanding_invoices)
-        return res
-
-    def assign_credit_invoices_to_outstanding_invoices(self):
-        for credit_invoice in self.line_payment_invoice_debit_ids:
-            associated_invoices = []
-            if credit_invoice.selected:
-                payment_term_line_id = credit_invoice.invoice_id.line_ids.filtered(
-                    lambda l: l.display_type == 'payment_term')
-                for outstanding_invoice in self.get_unprocessed_outstanding_invoices():
-                    outstanding_invoice.invoice_id.js_assign_outstanding_line(payment_term_line_id.id)
-                    associated_invoices.append(outstanding_invoice.invoice_id.name)
-                    if credit_invoice.invoice_id.amount_residual == 0:
-                        _logger.info("Credit Invoice %s assigned to : %s" % (credit_invoice.invoice_id.name,
-                                                                             ', '.join(associated_invoices)))
-                        break
-
-    def get_unprocessed_outstanding_invoices(self):
-        return self.line_payment_invoice_credit_ids.filtered(lambda l: l.selected and l.invoice_id.amount_residual > 0)
-
-    def assign_payment_to_outstanding_invoices(self, outstanding_invoices):
-        payment_line_entry = self.env["account.move.line"].search([("partner_id", "=", self.partner_id.id),
-                                                                   ("payment_id", "=", self.id),
-                                                                   ("company_id", "=", self.company_id.id),
-                                                                   ("amount_residual", "<", 0.0),
-                                                                   ])
-        associated_invoices = []
-        for outstanding_invoice in outstanding_invoices:
-            outstanding_invoice.invoice_id.js_assign_outstanding_line(payment_line_entry.id)
-            associated_invoices.append(outstanding_invoice.invoice_id.name)
-            if payment_line_entry.amount_residual == 0:
-                _logger.info("Payment %s assigned to : %s" % (self.name,
-                                                              ', '.join(associated_invoices)))
-                break
-
-    @api.onchange('amount')
-    def paid_amount_onchange(self):
-        if self.is_auto_reconciliation_applicable:
-            self.balance_outstanding = self.total_receivable() - self.amount
-            paid_amt = self.amount + self.total_credit()
-            for line in self.line_payment_invoice_credit_ids:
-                line.remaining_amt = line.invoice_amt
-                line.amount = 0.00
-                line.selected = False
-                if line.remaining_amt >= paid_amt and paid_amt > 0:
-                    line.amount = paid_amt
-                    line.remaining_amt = line.remaining_amt - paid_amt
-                    line.selected = True
-                    paid_amt = 0
-                else:
-                    if line.remaining_amt < paid_amt:
-                        remaining_bal = 0
-                        paid_amt = paid_amt - line.remaining_amt
-                        line.amount = line.remaining_amt
-                        line.remaining_amt = remaining_bal
-                        line.selected = True
-
     @api.onchange('partner_id')
     def partner_id_onchange(self):
         if self.is_auto_reconciliation_applicable:
@@ -236,10 +128,118 @@ class AccountPayment(models.Model):
             return {'value': {'line_payment_invoice_credit_ids': outstanding_vals,
                               'line_payment_invoice_debit_ids': credit_vals}}
 
+    @api.onchange('amount')
+    def paid_amount_onchange(self):
+        if self.is_auto_reconciliation_applicable:
+            self.balance_outstanding = self.total_receivable() - self.amount
+            paid_amt = self.amount + self.total_credit()
+            for line in self.line_payment_invoice_credit_ids:
+                line.remaining_amt = line.invoice_amt
+                line.amount = 0.00
+                line.selected = False
+                if line.remaining_amt >= paid_amt and paid_amt > 0:
+                    line.amount = paid_amt
+                    line.remaining_amt = line.remaining_amt - paid_amt
+                    line.selected = True
+                    paid_amt = 0
+                else:
+                    if line.remaining_amt < paid_amt:
+                        remaining_bal = 0
+                        paid_amt = paid_amt - line.remaining_amt
+                        line.amount = line.remaining_amt
+                        line.remaining_amt = remaining_bal
+                        line.selected = True
+
+    def action_post(self):
+        res = super(AccountPayment, self).action_post()
+        if self.is_auto_reconciliation_applicable:
+            if len(self.line_payment_invoice_debit_ids) > 0:
+                self.assign_credit_invoices_to_outstanding_invoices()
+            unprocessed_outstanding_invoices = self.get_unprocessed_outstanding_invoices()
+            if unprocessed_outstanding_invoices and self.amount > 0 and self.payment_type == 'inbound':
+                self.assign_payment_to_outstanding_invoices(unprocessed_outstanding_invoices)
+        return res
+
     def action_draft(self):
         super(AccountPayment, self).action_draft()
         _logger.info("Default Payment Reset Flow completed. Resetting credit invoice allocations.")
         self.unlink_credit_invoice_associations()
+
+    def generate_report_action(self):
+        return self.env.ref("bahmni_multi_payment.account_summarized_multi_invoices_payment").report_action(self)
+
+    def multi_invoice_search(self):
+        """ Using ref find the invoice obj """
+        return self.env['account.move'].search(
+            [('move_type', '=', 'out_invoice'), ('amount_residual', '>', 0), ('partner_id', '=', self.partner_id.id)],
+            order="id desc", limit=1)
+
+    def cashier_name(self):
+        words = self.create_uid.name.split()
+        initials = ''.join(word[0].upper() for word in words)
+        return initials
+
+    def total_credit(self):
+        credit_value = 0.0
+        if self.partner_id:
+            self._cr.execute("""select sum(ABS(amount_residual)) from account_move where 
+                          (amount_residual < 0 or (move_type='out_refund' and amount_residual > 0)) and partner_id = %s
+                          """, (self.partner_id.id,))
+            result = self._cr.fetchall()
+            if result[0][0] is not None:
+                credit_value = result[0][0]
+            else:
+                credit_value = 0.00
+        return credit_value
+
+    def total_outstanding(self):
+        outstanding = 0.0
+        if self.partner_id:
+            self._cr.execute("""select sum(amount_residual) from account_move where 
+                              amount_residual > 0 and move_type='out_invoice' and partner_id = %s
+                              """, (self.partner_id.id,))
+            outstanding_value = self._cr.fetchall()
+            if outstanding_value[0][0] is not None:
+                outstanding = outstanding_value[0][0]
+            else:
+                outstanding = 0.00
+        return outstanding
+
+    def total_receivable(self):
+
+        return self.total_outstanding() - self.total_credit()
+
+    def assign_credit_invoices_to_outstanding_invoices(self):
+        for credit_invoice in self.line_payment_invoice_debit_ids:
+            associated_invoices = []
+            if credit_invoice.selected:
+                payment_term_line_id = credit_invoice.invoice_id.line_ids.filtered(
+                    lambda l: l.display_type == 'payment_term')
+                for outstanding_invoice in self.get_unprocessed_outstanding_invoices():
+                    outstanding_invoice.invoice_id.js_assign_outstanding_line(payment_term_line_id.id)
+                    associated_invoices.append(outstanding_invoice.invoice_id.name)
+                    if credit_invoice.invoice_id.amount_residual == 0:
+                        _logger.info("Credit Invoice %s assigned to : %s" % (credit_invoice.invoice_id.name,
+                                                                             ', '.join(associated_invoices)))
+                        break
+
+    def get_unprocessed_outstanding_invoices(self):
+        return self.line_payment_invoice_credit_ids.filtered(lambda l: l.selected and l.invoice_id.amount_residual > 0)
+
+    def assign_payment_to_outstanding_invoices(self, outstanding_invoices):
+        payment_line_entry = self.env["account.move.line"].search([("partner_id", "=", self.partner_id.id),
+                                                                   ("payment_id", "=", self.id),
+                                                                   ("company_id", "=", self.company_id.id),
+                                                                   ("amount_residual", "<", 0.0),
+                                                                   ])
+        associated_invoices = []
+        for outstanding_invoice in outstanding_invoices:
+            outstanding_invoice.invoice_id.js_assign_outstanding_line(payment_line_entry.id)
+            associated_invoices.append(outstanding_invoice.invoice_id.name)
+            if payment_line_entry.amount_residual == 0:
+                _logger.info("Payment %s assigned to : %s" % (self.name,
+                                                              ', '.join(associated_invoices)))
+                break
 
     def unlink_credit_invoice_associations(self):
         allocated_credit_invoices = self.line_payment_invoice_debit_ids.filtered(lambda l: l.selected)
