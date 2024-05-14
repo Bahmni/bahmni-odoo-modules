71d9f9b112dd1b16d1d5d3af4fc994193171b84c
MOHANKUMAR T <mohan13081999@gmail.com>
2024-05-14 16:45:46 +0530

    BAH-3736 | Add. Update product price details on purchase order confirmation

--- bahmni_stock/models/stock_move_line.py
+++ bahmni_stock/models/stock_move_line.py
@@ -5,39 +5,39 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError, AccessError, RedirectWarning
 
 class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'     
-    
+    _inherit = 'stock.move.line'
+
     sale_price = fields.Float(string="Sale Price")
     mrp = fields.Float(string="MRP")
-    cost_price = fields.Float(string="Cost Price")    
-    balance = fields.Float(string="Balance")    
+    cost_price = fields.Float(string="Cost Price")
+    balance = fields.Float(string="Balance")
     existing_lot_id = fields.Many2one(
         'stock.lot', 'Lot/Serial Number',
         domain="[('product_id', '=', product_id), ('company_id', '=', company_id)]", check_company=True)
-    
+
     @api.onchange('product_id','qty_done')
-    def _onchange_balance_qty(self): 
+    def _onchange_balance_qty(self):
         if self.location_id and self.product_id:
             self.balance = (sum([stock.inventory_quantity_auto_apply for stock in self.env['stock.quant'].search([('location_id', '=', self.location_id.id),('product_id', '=', self.product_id.id)])])) - self.qty_done
-    
+
     @api.onchange('existing_lot_id')
-    def _onchange_existing_lot_id(self):        
-        if self.existing_lot_id:                
-           self.lot_name = self.existing_lot_id.name 
-           self.expiration_date = self.existing_lot_id.expiration_date 
+    def _onchange_existing_lot_id(self):
+        if self.existing_lot_id:
+           self.lot_name = self.existing_lot_id.name
+           self.expiration_date = self.existing_lot_id.expiration_date
         else:
-            self.lot_name = '' 
+            self.lot_name = ''
             self.expiration_date = fields.Datetime.today() + datetime.timedelta(days=self.product_id.expiration_time)
-    
+
     @api.constrains('mrp')
     def _check_fields_values(self):
         for rec in self:
-            if rec.mrp > 0.00:        
+            if rec.mrp > 0.00:
                 if rec.sale_price > rec.mrp:
                     raise ValidationError('Sales Price should not be greater than MRP Rate.')
             else:
                 pass
-                
+
     @api.onchange('cost_price')
     def _check_cost_price_values(self):
         if self.cost_price > 0.00:
@@ -59,7 +59,7 @@ class StockMoveLine(models.Model):
             else:
                 pass
         return res
-        
+
     def _create_and_assign_production_lot(self):
         """ Creates and assign new production lots for move lines."""
         lot_vals = []
@@ -68,7 +68,6 @@ class StockMoveLine(models.Model):
         key_to_index = {}  # key to index of the lot
         key_to_mls = defaultdict(lambda: self.env['stock.move.line'])  # key to all mls
         for ml in self:
-            ml.product_id.write({'standard_price': ml.cost_price,'list_price': ml.sale_price,'mrp': ml.mrp})
             key = (ml.company_id.id, ml.product_id.id, ml.lot_name, ml.cost_price, ml.sale_price, ml.mrp, ml.expiration_date)
             key_to_mls[key] |= ml
             if ml.tracking != 'lot' or key not in key_to_index:
@@ -78,10 +77,10 @@ class StockMoveLine(models.Model):
         for key, mls in key_to_mls.items():
             lot = lots[key_to_index[key]].with_prefetch(lots._ids)   # With prefetch to reconstruct the ones broke by accessing by index
             mls.write({'lot_id': lot.id})
-            
+
     def _get_value_production_lot(self):
         self.ensure_one()
-        
+
         return {
             'company_id': self.company_id.id,
             'name': self.lot_name,
@@ -91,4 +90,4 @@ class StockMoveLine(models.Model):
             'mrp': self.mrp,
             'expiration_date': self.expiration_date
         }
-    
+
