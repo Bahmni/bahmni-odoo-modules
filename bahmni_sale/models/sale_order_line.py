5efb96f359cfff71fea9811e484445c5b3e59ccd
srijanram <srijan.ram@thoughtworks.com>
2026-07-10 12:29:11 +0530

    BAH-4861 | Add. Adding index for external_order_id in sale_order_line

--- bahmni_sale/models/sale_order_line.py
+++ bahmni_sale/models/sale_order_line.py
@@ -17,7 +17,8 @@ class SaleOrderLine(models.Model):
     external_id = fields.Char(string="External ID",
                               help="This field is used to store encounter ID of bahmni api call")
     external_order_id = fields.Char(string="External Order ID",
-                                    help="This field stores the order ID got from api call.")
+                                    help="This field stores the order ID got from api call.",
+                                    index=True)
     order_uuid = fields.Char(string="Order UUID",
                              help="Field for generating a random unique ID.")
     dispensed = fields.Boolean(string="Dispensed",
